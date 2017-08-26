8700b3afcdf6444fbbec314985dca109137b938d
JulianG97 <julian.gamauf@fhwn.ac.at>
2017-08-26 15:55:21 +0200

    Settings class

--- Server/Program.cs
+++ Server/Program.cs
@@ -16,8 +16,7 @@ namespace Server
         private static bool serverRunning = false;
         private static TcpListener listener;
         private static UserRequestManager userRequestManager = new UserRequestManager();
-        private static int serverPort;
-        private static string serverUserPath;
+        private static Settings settings = new Settings();
 
         public static void Main(string[] args)
         {
@@ -80,9 +79,9 @@ namespace Server
             }
             else if (serverRunning == false)
             {
-                CheckConfigFile();
+                settings.CheckConfigFile();
 
-                listener = new TcpListener(IPAddress.Any, serverPort);
+                listener = new TcpListener(IPAddress.Any, settings.ServerPort);
 
                 Thread startServer = new Thread(StartListening);
 
@@ -106,7 +105,7 @@ namespace Server
             }
             else if (serverRunning == false)
             {
-                Console.WriteLine("The server has been stopped already! You can't stop the server again!");
+                Console.WriteLine("The server isn't running! You can't stop the server!");
             }
         }
 
@@ -139,102 +138,7 @@ namespace Server
             {
                 string userRequest = NetworkManager.ReadMessage(client, 28);
 
-                userRequestManager.HandleUserRequest(userRequest, client, serverUserPath);
-            }
-        }
-
-        public static void CheckConfigFile()
-        {
-            int port;
-            string userPath;
-
-            string configPath = Directory.GetCurrentDirectory();
-            configPath = configPath + @"\config.txt";
-
-            if (File.Exists(configPath) == true)
-            {
-                string[] configContent = File.ReadAllLines(configPath);
-
-                CheckConfigPort(configContent[0], out port);
-
-                if (port == 0)
-                {
-                    GenerateDefaultConfigFile();
-                }
-                else if (port != 0)
-                {
-                    CheckConfigUserPath(configContent[1], out userPath);
-
-                    if (userPath == string.Empty)
-                    {
-                        GenerateDefaultConfigFile();
-                    }
-                    else if (userPath != string.Empty)
-                    {
-                        serverPort = port;
-                        serverUserPath = userPath;
-                    }
-                }
-            }
-            else if (File.Exists(configPath) == false)
-            {
-                GenerateDefaultConfigFile();
-            }
-        }
-
-        public static void GenerateDefaultConfigFile()
-        {
-            string configPath = Directory.GetCurrentDirectory();
-
-            string[] configFile = new string[2];
-
-            configFile[0] = "Port: 80";
-            configFile[1] = "User Path: " + configPath + @"\User";
-
-            File.WriteAllLines(configPath + @"\config.txt", configFile);
-
-            if (Directory.Exists(configPath + @"\User") == false)
-            {
-                Directory.CreateDirectory(configPath + @"\User");
-            }
-        }
-
-        public static void CheckConfigPort(string portLine, out int port)
-        {
-            char[] portArray = portLine.ToCharArray();
-            port = 0;
-
-            if (portArray[0] == 'P' && portArray[1] == 'o' && portArray[2] == 'r' && portArray[3] == 't' && portArray[4] == ':' && portArray[5] == ' ')
-            {
-                string portString = string.Empty;
-
-                for (int i = 6; i < portArray.Length; i++)
-                {
-                    portString = portString + portArray[i];
-                }
-
-                int.TryParse(portString, out port);
-            }
-        }
-
-        public static void CheckConfigUserPath(string userPathLine, out string userPath)
-        {
-            char[] userPathArray = userPathLine.ToCharArray();
-            userPath = string.Empty;
-
-            if (userPathArray[0] == 'U' && userPathArray[1] == 's' && userPathArray[2] == 'e' && userPathArray[3] == 'r' && userPathArray[4] == ' ' && userPathArray[5] == 'P' && userPathArray[6] == 'a' && userPathArray[7] == 't' && userPathArray[8] == 'h' && userPathArray[9] == ':' && userPathArray[10] == ' ')
-            {
-                string userPathString = string.Empty;
-
-                for (int i = 11; i < userPathArray.Length; i++)
-                {
-                    userPathString = userPathString + userPathArray[i];
-                }
-
-                if (Directory.Exists(userPathString) == true)
-                {
-                    userPath = userPathString;
-                }
+                userRequestManager.HandleUserRequest(userRequest, client, settings.ServerUserPath);
             }
         }
     }
---
+++ Server/Settings.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Server
+{
+    public class Settings
+    {
+        public int ServerPort
+        {
+            get;
+            set;
+        }
+
+        public string ServerUserPath
+        {
+            get;
+            set;
+        }
+
+        public void CheckConfigFile()
+        {
+            int port;
+            string userPath;
+
+            string configPath = Directory.GetCurrentDirectory();
+            configPath = configPath + @"\config.txt";
+
+            if (File.Exists(configPath) == true)
+            {
+                string[] configContent = File.ReadAllLines(configPath);
+
+                this.CheckConfigPort(configContent[0], out port);
+
+                if (port == 0)
+                {
+                    this.GenerateDefaultConfigFile();
+                }
+                else if (port != 0)
+                {
+                    this.CheckConfigUserPath(configContent[1], out userPath);
+
+                    if (userPath == string.Empty)
+                    {
+                        this.GenerateDefaultConfigFile();
+                    }
+                    else if (userPath != string.Empty)
+                    {
+                        ServerPort = port;
+                        ServerUserPath = userPath;
+                    }
+                }
+            }
+            else if (File.Exists(configPath) == false)
+            {
+                this.GenerateDefaultConfigFile();
+            }
+        }
+
+        public void GenerateDefaultConfigFile()
+        {
+            string configPath = Directory.GetCurrentDirectory();
+
+            string[] configFile = new string[2];
+
+            configFile[0] = "Port: 80";
+            configFile[1] = "User Path: " + configPath + @"\User";
+
+            File.WriteAllLines(configPath + @"\config.txt", configFile);
+
+            if (Directory.Exists(configPath + @"\User") == false)
+            {
+                Directory.CreateDirectory(configPath + @"\User");
+            }
+        }
+
+        public void CheckConfigPort(string portLine, out int port)
+        {
+            char[] portArray = portLine.ToCharArray();
+            port = 0;
+
+            if (portArray[0] == 'P' && portArray[1] == 'o' && portArray[2] == 'r' && portArray[3] == 't' && portArray[4] == ':' && portArray[5] == ' ')
+            {
+                string portString = string.Empty;
+
+                for (int i = 6; i < portArray.Length; i++)
+                {
+                    portString = portString + portArray[i];
+                }
+
+                int.TryParse(portString, out port);
+            }
+        }
+
+        public void CheckConfigUserPath(string userPathLine, out string userPath)
+        {
+            char[] userPathArray = userPathLine.ToCharArray();
+            userPath = string.Empty;
+
+            if (userPathArray[0] == 'U' && userPathArray[1] == 's' && userPathArray[2] == 'e' && userPathArray[3] == 'r' && userPathArray[4] == ' ' && userPathArray[5] == 'P' && userPathArray[6] == 'a' && userPathArray[7] == 't' && userPathArray[8] == 'h' && userPathArray[9] == ':' && userPathArray[10] == ' ')
+            {
+                string userPathString = string.Empty;
+
+                for (int i = 11; i < userPathArray.Length; i++)
+                {
+                    userPathString = userPathString + userPathArray[i];
+                }
+
+                if (Directory.Exists(userPathString) == true)
+                {
+                    userPath = userPathString;
+                }
+            }
+        }
+    }
+}
